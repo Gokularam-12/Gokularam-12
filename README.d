01b0918291172a2545ea215de7f965d183b29c2c
Gokularam M <gokularamrm@gmail.com>
2026-04-27 19:57:58 +0530

    Update README.d

--- README.d
+++ README.d
@@ -15,7 +15,7 @@
 
 <h3 align="left">Connect with me:</h3>
 <p align="left">
-<a href="https://linkedin.com/in/gokularam m" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="gokularam m" height="30" width="40" /></a>
+<a href="www.linkedin.com/in/gokularamrm" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="gokularam m" height="30" width="40" /></a>
 <a href="https://www.leetcode.com/gokularam" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="gokularam" height="30" width="40" /></a>
 <a href="https://www.topcoder.com/members/630610" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/topcoder.svg" alt="630610" height="30" width="40" /></a>
 </p>
